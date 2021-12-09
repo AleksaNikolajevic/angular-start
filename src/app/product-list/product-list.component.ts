3e40a65e4ec6d923b71ff4a54079ae1726ed8f07
AleksaNikolajevic <75322640+AleksaNikolajevic@users.noreply.github.com>
2021-12-09 23:57:41 +0100

    loll

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ProductAlertsComponent } from "../product-alerts/ProductAlertsComponent";
 import { products } from '../products';
 
 @Component({
